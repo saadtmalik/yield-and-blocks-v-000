e80889bd470ec5cca951dce535b7ceb9c8ee0f87
Saad Malik <saad.t.malik@gmail.com>
2018-11-12 18:04:49 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,7 @@
 def hello_t(array)
   i = 0
   while i < array.length
-    yield array [i] #this brings up successive value stored in the array
+    yield array [i] #this brings up successive value stored in the array, notice []
     i = i + 1
   end
 end
